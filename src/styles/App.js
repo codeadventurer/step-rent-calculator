33470a0d0d282f69211a4a9702077dddb41a2956
Anna Dontcova <a.dontcova@gmail.com>
2021-03-23 19:11:18 +0100

    fixes jumping style

--- src/styles/App.js
+++ src/styles/App.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  margin-top: 5rem;
   align-items: center;
   height: 100vh;
-`;
+`
